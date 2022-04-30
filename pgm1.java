9f06d023b20eece002c0f6266a8111cb79f7905a
AbhiSince2k <96220514+AbhiSince2k@users.noreply.github.com>
2022-04-30 17:32:48 +0530

    Add files via upload

---
+++ pgm1.java
@@ -0,0 +1,75 @@
+package pgm1;
+
+public class pgm1 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("HELLO! FROM ABHI_SINCE_2K");//This is print5ing line
+		/*Line Comment-1
+		 * Line Comment-2
+		 */
+//Create a variable named carName and assign the value Volvo to it.
+		String carNameString="Volvo";
+//Create a variable named maxSpeed and assign the value 120 to it.	
+		int maxSpeed=120;
+//Display the sum of 5 + 10, using two variables: x and y.
+		int x=5;
+		int y=10;
+		sum=x+y;//if sum not apply then other method we used in...
+		System.out.println("Sum of: " +sum);
+		/* OR
+		 * System.out.println(x+y);
+		 */
+//Create a variable called z, assign x + y to it, and display the result.
+		int x=5;
+		int y=10;
+		int z=x+y;
+		System.out.println(x+y);
+		
+//Create three variables of the same type, using a comma-separated list.
+		int x=5,y=10,z=30;
+		System.out.println(x+y+z);
+//Add the correct data type for the following variables:
+int myNum = 9;
+float myFloatNum = 8.99f;
+char myLetter = 'A';
+boolean myBool = false;
+String myText = "Hello World";
+		
+//byte, short, int, long, float, double, boolean and char are called as
+//-->:[Primitive Data type]
+//Multiply 10 with 5, and print the result.
+//Divide 10 by 5, and print the result.
+	int x=10, y=5;
+	System.out.println(x*y);
+	System.out.println(x/y);
+
+//Use the correct operator to increase the value of the variable x by 1.
+	int x=20;
+	++x;
+//Use the addition assignment operator to add the value 5 to the variable x.
+	int x=17;
+	x+=5;
+//Use the correct operator to decrease the value of the variable x by 1.
+	int x;
+	x=10;
+	x=x-1;
+	
+//Use the subtraction assignment operator to subtract the value 5 to the variable x.
+	int x;
+	x=10;
+	x=x-5;
+//Use the multiplication assignment operator to multply the value 5 to the variable x.
+	int x;
+	x=10;
+	x=x*5;
+//Using instance variable perform Arithmetic Operation, Relational Operation
+
+	
+//Create final and Static variables and print in main method.
+	
+//Create public, private, protected and default variables and print in the classes according to its rules.		
+
+	}
+
+}
